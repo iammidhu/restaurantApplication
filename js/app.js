58d0e900e6b6dc40ddf7b19cfcbfbf501d0ae6b2
gokulk94 <gokulk@qburst.com>
2016-02-08 18:19:23 +0530

    worked on the first page of the assignment.

--- js/app.js
+++ js/app.js
@@ -1,51 +1,19 @@
-var myApp = angular.module('myApp',['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute']);
+
 
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
-        when('/home', {
-            templateUrl: 'view/home.html',
-            controller: 'homeController'
-        }).
-        when('/', {
-            templateUrl: 'view/result.html',
-            controller: 'resultController'
-        }).
-        when('/checkout', {
-            templateUrl: 'view/checkout.html',
-            controller: 'checkoutController'
-        })
-        $locationProvider.html5Mode(true);
-}]);
-
-myApp.directive('myMap',['mapUtils', function(mapUtils) {
-    // directive link function
-    var link = function(scope, element, attrs) {
-        var map, infoWindow;
-        var markers = [];
-
-        // map config
-        var mapOptions = {
-            center: new google.maps.LatLng(50, 2),
-            zoom: 4,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            scrollwheel: false
-        };
-
-        // init the map
-
-
-        // show the map and place some markers
-        mapUtils.initMap();
-
-        mapUtils.setMarker(map, new google.maps.LatLng(51.508515, -0.125487), 'London', 'Just some content');
-        mapUtils.setMarker(map, new google.maps.LatLng(52.370216, 4.895168), 'Amsterdam', 'More content');
-        mapUtils.setMarker(map, new google.maps.LatLng(48.856614, 2.352222), 'Paris', 'Text here');
-    };
-
-    return {
-        restrict: 'A',
-        template: '<div id="gmaps"></div>',
-        replace: true,
-        link: link
-    };
+    when('/', {
+        templateUrl: 'view/home.html',
+        controller: 'homeController'
+    }).
+    when('/result', {
+        templateUrl: 'view/result.html',
+        controller: 'resultController'
+    }).
+    when('/checkout', {
+        templateUrl: 'view/checkout.html',
+        controller: 'checkoutController'
+    })
+    $locationProvider.html5Mode(true);
 }]);
--- js/services.js
+++ js/services.js
@@ -1,45 +1,29 @@
-myApp.factory('mapUtils', function() {
-    return {
-        initMap: function() {
-            if (map === void 0) {
-                map = new google.maps.Map(element[0], mapOptions);
+app.factory('searchQuery', function() {
+    var search = function(data) {
+        var config = {
+            url: 'json/list.json',
+            method: 'GET'
+        };
+
+        var list,
+            searchResult = [];
+
+        $http(config).then(function(response) {
+            list = response.data;
+        }, function() {
+            console.log('Error occured when getting json file');
+        });
+
+        list = JSON.parse(list);
+        for(i=0 ; i < list.length ; i++) {
+            if(list[i].indexOf(data) > -1) {
+                searchResult.push(list[i]);
             }
-        },
-
-        // place a marker
-        setMarker: function(map, position, title, content) {
-            var marker;
-            var markerOptions = {
-                position: position,
-                map: map,
-                title: title,
-                icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
-            };
-
-            marker = new google.maps.Marker(markerOptions);
-            markers.push(marker); // add marker to array
-
-            google.maps.event.addListener(marker, 'click', function() {
-                // close window if not undefined
-                if (infoWindow !== void 0) {
-                    infoWindow.close();
-                }
-                // create new window
-                var infoWindowOptions = {
-                    content: content
-                };
-                infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-                infoWindow.open(map, marker);
-            });
         }
-    }
-});
 
+        return searchResult;
 
+    };
 
-
-myApp.factory('submitData',function() {
-    return function(data,scope) {
-        scope.datas.push(data);
-    }
+    return search;
 });
