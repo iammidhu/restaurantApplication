b459dfa14ce93c488cd8d7e6f92da92e0ced201b
midhun <iammidhunpj@gmail.com>
2016-02-10 18:51:45 +0530

    Adding menu to order food

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -1,7 +1,7 @@
 myApp.controller('searchController', ['$scope', '$location', function($scope, $location) {
   $scope.submitHome = function(data) {
     if(data) {
-      console.log(data);
+      
       // var next = sharedData;
       // next.set(data);
       $location.path('/search/'+data);
