294516f760a04d2b904fb6de7759716f56b9c583
gokulk94 <gokulk@qburst.com>
2016-02-10 11:22:47 +0530

    for pull

--- js/controller.js
+++ js/controller.js
@@ -41,26 +41,45 @@ myApp.controller('homeController', ['$scope', 'searchQuery', 'createMarker', 'sh
     $scope.nextScreen = function(data) {
         var next = sharedData;
         next.set(data);
+        return true;
     }
 }]);
 
 myApp.controller('resultController', ['$scope', 'sharedData', 'getMenuList', function($scope, sharedData, getMenuList) {
-    var next = sharedData;
-    var data = next.get();
-    console.log(data);
-    $scope.data=data;
-    $scope.page = "menu";
     //get menu json via http passing the id.
-    var jsonRequest = getMenuList(data.id);
-    jsonRequest.then(function(response) {
-        $scope.menuList = response;
-        console.log($scope.menuList);
-    });
     //two tabs: info tab and the menu tab.
     //info tab displays all the info about the restaurant.
     //the menu tab lists the menu items with a checkbox.
     //when checked, the checkout box on the right is dynamically updated.
     //when checkout is clicked, user is redirected to the checkout page.
+    var next = sharedData;
+    var data = next.get();
+    console.log(data);
+    $scope.data=data;
+    $scope.page = "menu";
+    $scope.review = [];
+    var jsonRequest = getMenuList(data.id);
+    jsonRequest.then(function(response) {
+        $scope.menuList = response;
+        console.log($scope.menuList);
+    });
+    var addItem = function(data, count) {
+        $scope.review.push({
+            item: data.name,
+            count: count,
+            pricePerItem: data.price,
+            total: (count*data.price)
+        });
+    };
+
+    var checkoutClick = function(data) {
+        if(data.length > 0) {
+            next.set(data);
+            return true;
+        } else {
+            return false;
+        }
+    };
 }]);
 
 myApp.controller('checkoutController', ['$scope', function($scope) {
