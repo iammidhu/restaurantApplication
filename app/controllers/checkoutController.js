d6dd5af4484756db6d7fd151108ba18c1a798722
gokulk94 <gokulk@qburst.com>
2016-02-11 18:45:31 +0530

    Worked on the checkout page

--- app/controllers/checkoutController.js
+++ app/controllers/checkoutController.js
@@ -1,3 +1,6 @@
-myApp.controller('checkoutController', ['$scope', function($scope) {
+myApp.controller('checkoutController', ['$scope', 'sharedData', function($scope, sharedData) {
     //Here the summary of the order is displayed along with a select payment option.
+    var next = sharedData;
+    var data = next.get();
+    $scope.info = data;
 }]);
