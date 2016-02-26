507edde7b76f168b62c194d362954082d894c1db
midhun <iammidhunpj@gmail.com>
2016-02-26 17:38:19 +0530

    success page partially completed

--- app/controllers/successController.js
+++ app/controllers/successController.js
@@ -1,7 +1,16 @@
 myApp.controller('successController', ['$scope', '$location', 'finalData', function($scope, $location, finalData) {
 
-      var next = finalData;
-      var orderData = next.get();
+  var next = finalData;
+  var orderData = next.get();
 
-      $scope.total = orderData.data.total;
-    }]);
+  $scope.total = orderData.data.total;
+
+  $scope.delivery_type = orderData.delivery.name;
+  $scope.showVariable = true;
+
+  if ($scope.delivery_type == 'pickup') {
+    $scope.showVariable = false;
+  } else {
+    $scope.showVariable = true;
+  }
+}]);
